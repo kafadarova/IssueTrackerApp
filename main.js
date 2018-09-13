dc4fa5ee97a8c607fdb7bbded9c574d665efb027
kafadarova <k.kafadarova@gmail.com>
2018-09-13 23:47:28 +0200

    create and assign variables for the issues and than create an object

--- main.js
+++ main.js
@@ -35,6 +35,18 @@ document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 
 function saveIssue(e) {
   var issueId = chance.guid();
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issueSeverity = document.getElementById('issueSeverityInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  var issueStatus = 'Open';
+
+  var issue = {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+    status: issueStatus
+  }
 }
 //   if(localStorage.getItem('issues') === null) {
 //     var issues = [];
